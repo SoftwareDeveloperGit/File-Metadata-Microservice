f7019e9ac48dd66b1f807235cbbfd25c404cdc86
Rohit Kumar <187720712+SoftwareDeveloperGit@users.noreply.github.com>
2024-11-13 22:51:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var cors = require('cors');
-const multer = require('multer');
-const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
 var app = express();
@@ -9,27 +8,19 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-/* File Metadata Microcervice API */
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  if (!req.file) {
-    return res.json({ error: 'No file selected' });
-  }
-
-  const { originalname, mimetype, size } = req.file;
-  res.json({ name: originalname, type: mimetype, size: size });
-});
-/* File Metadata Microcervice API */
-
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Your App is Listening on Port ' + port)
+app.listen(port, function () {
+    console.log('Your app is listening on port ' + port)
 });
